5f989b12a74c50f678075a07051a6fa35f1d493a
Lucy Kim <lucy.kim@everyonebeauty.com>
2019-06-25 15:00:59 +0900

    cors 설정 추가

---
+++ src/main/kotlin/io/eb/svr/handler/controller/AdminController.kt
@@ -0,0 +1,35 @@
+package io.eb.svr.handler.controller
+
+import io.eb.svr.common.config.ApiConfig.ADMIN_PATH
+import io.eb.svr.common.config.ApiConfig.API_VERSION
+import io.eb.svr.common.config.ApiConfig.RECEPT_PATH
+import io.eb.svr.common.config.ApiConfig.SEARCH_PATH
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Create by lucy on 2019-06-25
+ **/
+@RestController
+@RequestMapping(
+	path = ["/${API_VERSION}/$ADMIN_PATH"]
+)
+class AdminController {
+
+//	@GetMapping(
+//		path = ["/$SEARCH_PATH/$RECEPT_PATH"],
+//		consumes = [MediaType.APPLICATION_JSON_VALUE],
+//		produces = [MediaType.APPLICATION_JSON_VALUE]
+//	)
+//	fun getReceptShopAll(
+//		servlet: HttpServletRequest
+//	) = ResponseEntity.status(HttpStatus.OK).body(authService.b2bLogin(servlet, request))
+//
+//	@PostMapping(
+//		path = ["/"]
+//	)
+
+}
--- src/main/kotlin/io/eb/svr/security/WebConfig.kt
+++ src/main/kotlin/io/eb/svr/security/WebConfig.kt
@@ -1,21 +1,26 @@
 package io.eb.svr.security
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 /**
  * Create by lucy on 2019-06-05
  **/
-//@Configuration
-//class WebConfig {
-//	private val MAX_AGE_SECS: Long = 3600
-//
-//	@Bean
-//	fun corsConfigurer(): WebMvcConfigurer {
-//		return object : WebMvcConfigurer {
-//			override fun addCorsMappings(registry: CorsRegistry) {
-//				registry.addMapping("/**")
-//					.allowedOrigins("*")
-//					.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-//					.maxAge(MAX_AGE_SECS)
-//			}
-//		}
-//	}
-//}
+@Configuration
+class WebConfig {
+	private val MAX_AGE_SECS: Long = 3600
+
+	@Bean
+	fun corsConfigurer(): WebMvcConfigurer {
+		return object : WebMvcConfigurer {
+			override fun addCorsMappings(registry: CorsRegistry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+					.maxAge(MAX_AGE_SECS)
+			}
+		}
+	}
+}
